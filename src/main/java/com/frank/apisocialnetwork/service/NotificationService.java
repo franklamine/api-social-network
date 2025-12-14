67cb36f54fb50716000fcb95ab8b6ad81610a220
FRANK-PC\frank <2400616@cegeplimoilou.ca>
2025-12-14 05:35:56 -0500

    implementation de l'envoi des mail par gmail et ajustement de docker compose

--- src/main/java/com/frank/apisocialnetwork/service/NotificationService.java
+++ src/main/java/com/frank/apisocialnetwork/service/NotificationService.java
@@ -14,7 +14,7 @@ public class NotificationService {
     JavaMailSender javaMailSender;
     public void envoyerNotification(Validation validation) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("no-reply@frank.tech");
+        message.setFrom("fokamfranklamine@gmail.com");
         message.setTo(validation.getUtilisateur().getEmail());
         message.setSubject("Code de vérification");
         message.setText("Bonjour votre code de vérification est: " + validation.getCode());
